2a21a6c79a92b99a4e3727bac3e9e60b67cc4b94
Ankitsaini406 <as.ankitsaini406@gmail.com>
2024-05-15 17:47:41 +0530

    ""

--- src/components/Grid/CellComponent.jsx
+++ src/components/Grid/CellComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CellComponent = ({ filled, onClick, isDisabled }) => {
     return (
-        <button type='button' onClick={onClick} disabled={isDisabled} className={filled ? `cell cell-active` : `cell`}>
+        <button type='button' onClick={onClick} disabled={isDisabled} className={filled ? `grid-cell cell-active` : `grid-cell`}>
 
         </button>
     )
