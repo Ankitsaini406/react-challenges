e68a07a7f27e6a2c7f3f2be0ab4315b34acd4f66
Ankitsaini406 <as.ankitsaini406@gmail.com>
2024-05-19 11:34:03 +0530

    "TrafficLight"

--- src/components/TrafficLight/TrafficLight.jsx
+++ src/components/TrafficLight/TrafficLight.jsx
@@ -2,33 +2,33 @@ import { useEffect, useState } from "react";
 import "../../css/trafficligt.css";
 
 const lights = [
-    { color: "red", time: 4000, netLight: 1 },
-    { color: "yellow", time: 5000, netLight: 2 },
-    { color: "green", time: 3000, netLight: 0 },
+  { color: "red", time: 4000, netLight: 1 },
+  { color: "yellow", time: 5000, netLight: 2 },
+  { color: "green", time: 3000, netLight: 0 },
 ];
 
 const TrafficLight = () => {
-    const [activeLight, setActiveLight] = useState(0);
+  const [activeLight, setActiveLight] = useState(0);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setActiveLight(lights[activeLight].netLight)
-        }, lights[activeLight].time);
-    }, [activeLight]);
+  useEffect(() => {
+    setTimeout(() => {
+      setActiveLight(lights[activeLight].netLight);
+    }, lights[activeLight].time);
+  }, [activeLight]);
 
-    return (
-        <div className="traffic-box">
-            {lights.map((light, i) => {
-                const isActive = activeLight === i ? true : false;
-                return (
-                    <div
-                        className="light-cricul"
-                        style={{ backgroundColor: isActive ? light.color : "" }}
-                    ></div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="traffic-box">
+      {lights.map((light, i) => {
+        const isActive = activeLight === i ? true : false;
+        return (
+          <div
+            className="light-cricul"
+            style={{ backgroundColor: isActive ? light.color : "" }}
+          ></div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TrafficLight;
