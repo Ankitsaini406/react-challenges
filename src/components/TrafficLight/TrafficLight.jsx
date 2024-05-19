5949cc37b127a0eaaa44fe5b63d8b7bf71d2376d
Ankitsaini406 <as.ankitsaini406@gmail.com>
2024-05-19 11:33:34 +0530

    "TrafficLight"

--- src/components/TrafficLight/TrafficLight.jsx
+++ src/components/TrafficLight/TrafficLight.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import "../../css/trafficligt.css";
 
+const lights = [
+    { color: "red", time: 4000, netLight: 1 },
+    { color: "yellow", time: 5000, netLight: 2 },
+    { color: "green", time: 3000, netLight: 0 },
+];
+
 const TrafficLight = () => {
+    const [activeLight, setActiveLight] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setActiveLight(lights[activeLight].netLight)
+        }, lights[activeLight].time);
+    }, [activeLight]);
+
     return (
-    <div className="traffic-box">
-        <div className="light-cricul"></div>
-        <div className="light-cricul"></div>
-        <div className="light-cricul"></div>
-    </div>
+        <div className="traffic-box">
+            {lights.map((light, i) => {
+                const isActive = activeLight === i ? true : false;
+                return (
+                    <div
+                        className="light-cricul"
+                        style={{ backgroundColor: isActive ? light.color : "" }}
+                    ></div>
+                );
+            })}
+        </div>
     );
 };
 
